e2d66bd61032038ad483df3e48b666281aa3fdf1
suraj448 <nayaksuraj448@gmail.com>
2018-04-30 22:17:12 +0530

    test

---
+++ src/com/ey/test/Test.java
@@ -0,0 +1,20 @@
+package com.ey.test;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Heloo welcome back....");
+		
+	/*	List list = Collections.nCopies(3, "Suraj");
+		
+		Iterator i =list.iterator();
+		while(i.hasNext()){
+			System.out.println("list is :" +i.next());
+		}*/
+		
+	}
+}
